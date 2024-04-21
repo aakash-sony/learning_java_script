75801c2e1e03b5cf49ff845a05c4891a4720dc94
Akash <aakashsoni8756@gmail.com>
2024-04-21 23:41:21 +0530

    Push and Pop method

---
+++ addElementInArray.js
@@ -0,0 +1,8 @@
+
+//Add a numer in array
+let arr=[1,3,4,2,5,6,90]
+let a = 20;
+arr.push(a); //it will add a number in the array in last cell
+console.log(arr);
+arr.pop();//it will delete a number in the array from last cell
+console.log(arr);
